a64ddb0cb0200f94803d926a97725756145d54a1
James O.o <jamesoosagie@gmail.com>
2024-05-06 17:30:31 -0700

    fggg

--- input_handler.py
+++ input_handler.py
@@ -2,26 +2,24 @@ import pygame
 class InputHandler:
     def __init__(self):
         self.simulated_keys = {
-            "left": False,
-            "right": False,
-            "up": False,
-            "down": False
+            pygame.K_LEFT: False,
+            pygame.K_RIGHT: False,
+            pygame.K_UP: False,
+            pygame.K_DOWN: False
         }
 
-    def update_simulated_input(self, direction, state):
+    def update_simulated_input(self, key):
         """ Update the state of simulated inputs """
-        if direction in self.simulated_keys:
-            self.simulated_keys[direction] = state
+        print (f"Key pressed: {self.simulated_keys[key]}")
+        for k in self.simulated_keys:
+            self.simulated_keys[k] = (k == key)
 
     def get_key(self, key):
-        # Map Pygame keys to directions
-        key_map = {
-            pygame.K_LEFT: "left",
-            pygame.K_RIGHT: "right",
-            pygame.K_UP: "up",
-            pygame.K_DOWN: "down"
-        }
-        if key in key_map:
-            # Return the real key press or the simulated state
-            return pygame.key.get_pressed()[key] or self.simulated_keys[key_map[key]]
-        return False
+        if key in self.simulated_keys:
+            if pygame.key.get_pressed()[key]:
+                self.update_simulated_input(key)
+
+        return self.simulated_keys[key]
+
+    def get_pressed(self):
+        return self.simulated_keys
